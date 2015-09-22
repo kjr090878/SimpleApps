2a5e19f828f9ce981e6412cd71292a79079d7e03
Kelly Robinson <kellyrobinson@Kellys-MacBook-Pro.local>
2015-09-22 18:02:28 -0400

    Calculator Updates

--- Calculater/Calculater/ViewController.swift
+++ Calculater/Calculater/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    @IBOutlet weak var operationLabel: UILabel!
     
-
     @IBAction func numberPressed(button: UIButton) {
         
         let num = button.titleLabel!.text!
@@ -41,36 +41,75 @@ class ViewController: UIViewController {
         
         number1 = ""
         number2 = ""
+        
         operation = nil
         displayLabel.text = "0"
+        
+        operationLabel.text = nil
+        
     }
     
     @IBAction func changeSign () {
         
         
+        if operation == nil {
+            
+            let n1 = Double(number1)! * -1
+            number1 = "\(n1)"
+            displayLabel.text = number1
+            
+        } else {
+            
+            let n2 = Double(number2)! * -1
+            number2 = "\(n2)"
+            displayLabel.text = number2
+            
+        }
+        
+    
+        
     }
     
     @IBAction func setOperation(button: UIButton) {
         
-        let opSign = button.titleLabel!.text!
+        let opSign = button.titleLabel!.text! // "+"
         
         operation = OperationType(rawValue: opSign)
         
-        
-        
+        operationLabel.text = opSign
         
     }
     
     @IBAction func runOperation() {
         
+        // number1 to Double
         let num1 = Double(number1)
+        // number2 to Double
+        
         let num2 = Double(number2)
+        // total equal number1 operation to number2
         
         let total = calculateNumbers(n1: num1!, n2: num2!, operation: operation!)
         
+        // if calculations do not have a remainder then
+        
+        if total % 1 == 0 {
+            
+            //string is an integer and not Double
+            
+            displayLabel.text = "\(Int(total))" // 5
+            //or
+            
+        } else {
+            //string is Double
+            
+            displayLabel.text = "\(total)" // 5.5
+            
+        }
+        
         
         number1 = "\(total)"
-        displayLabel.text = "\(total)"
+        
         
         
     }
