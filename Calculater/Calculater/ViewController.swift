7b198af5113514260dd8a77c6dd6e6800f215ef0
Kelly Robinson <kellyrobinson@Kellys-MacBook-Pro.local>
2015-09-22 16:07:56 -0400

    Calculator Functions

--- Calculater/Calculater/ViewController.swift
+++ Calculater/Calculater/ViewController.swift
@@ -8,23 +8,71 @@
 
 import UIKit
 
+var number1 = ""
+var number2 = ""
+var operation: OperationType?
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
     
-    @IBOutlet weak var pressOne: UIButton!
-    @IBAction func pressPoint(sender: AnyObject) {
-    }
-    @IBAction func pressZero(sender: AnyObject) {
+    
+
+    @IBAction func numberPressed(button: UIButton) {
+        
+        let num = button.titleLabel!.text!
+        
+        if operation == nil {
+            
+            number1 += num
+            displayLabel.text = number1
+            
+        } else {
+            
+            number2 += num
+            displayLabel.text = number2
+            
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clear() {
+        
+        number1 = ""
+        number2 = ""
+        operation = nil
+        displayLabel.text = "0"
+    }
+    
+    @IBAction func changeSign () {
+        
+        
+    }
+    
+    @IBAction func setOperation(button: UIButton) {
+        
+        let opSign = button.titleLabel!.text!
+        
+        operation = OperationType(rawValue: opSign)
+        
+        
+        
+        
+    }
+    
+    @IBAction func runOperation() {
+        
+        let num1 = Double(number1)
+        let num2 = Double(number2)
+        
+        let total = calculateNumbers(n1: num1!, n2: num2!, operation: operation!)
+        
+        
+        number1 = "\(total)"
+        displayLabel.text = "\(total)"
+        
+        
     }
 
 
