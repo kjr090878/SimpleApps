7acc66846b0e5e1794cb7d8c015be01926ed6dde
Kelly Robinson <kellyrobinson@Kellys-MacBook-Pro.local>
2015-09-24 17:05:07 -0400

    StoryboardLess

---
+++ Calculater/BasicsContinued.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+///// Struct
+
+
+var name = "Jo"
+
+unsafeAddressOf(name)
+
+var name2 = name
+
+unsafeAddressOf(name2)
+
+name2.appendContentsOf("e")
+
+name
+
+/////Class
+
+var name3: NSMutableString = "Jo"
+
+unsafeAddressOf(name3)
+
+var name4 = name3
+
+unsafeAddressOf(name4)
+
+name4.appendString("e")
+name3
+
+
