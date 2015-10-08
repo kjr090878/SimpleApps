cd4e05d4d9b81c00d5347718564f83e610056e3b
Kelly Robinson <kjr090878@yahoo.com>
2015-10-08 17:26:13 -0400

    updates

---
+++ DrawSomething/Draw.swift
@@ -0,0 +1,25 @@
+//
+//  Draw.swift
+//  DrawSomething
+//
+//  Created by Kelly Robinson on 9/30/15.
+//  Copyright © 2015 Kelly Robinson. All rights reserved.
+//
+
+import UIKit
+
+class Draw: UIView {
+
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        
+        
+    }
+
+}
---
+++ DrawSomething/DrawSomething/DrawViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DrawViewController.swift
+//  DrawSomething
+//
+//  Created by Kelly Robinson on 9/29/15.
+//  Copyright © 2015 Kelly Robinson. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    
+    
+    
+    override func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        
+    }
+    func drawViewController(context: CGContextRef, boundedBy rect: CGRect) {
+    }
+    func drawViewController(context: CGContextRef, boundedBy rect: CGRect) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
