1baac9619398061bebcdd98915269e06a05bbffd
Kelly Robinson <kjr090878@yahoo.com>
2015-12-03 13:26:39 -0500

    touchyDot

---
+++ TouchyDot/TouchyDot/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  TouchyDot
+//
+//  Created by Kelly Robinson on 12/3/15.
+//  Copyright © 2015 Kelly Robinson. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController, ADBannerViewDelegate {
+    
+    
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var bannerView: ADBannerView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        bottomConstraint.constant = -bannerView.frame.height
+        
+        // create and run nstimer that will trigger pingDot
+        
+    }
+
+    // add button to storyboard for leaderboard
+    // add action for the leaderboard button
+    
+    // add label to storyboard for currentScore
+    
+
+
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bottomConstraint.constant = 0
+        
+        UIView.animateWithDuration(0.4) { () -> Void in
+            
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        
+        bottomConstraint.constant = -bannerView.frame.height
+        
+        UIView.animateWithDuration(0.4) { () -> Void in
+            
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
+
+    var correctDot = -1
+    var currentScore = 0
+    var dotTimer: NSTimer?
+
+    @IBAction func touchDot(dot: TouchButton) {
+    
+        // check if correctDot == dot.tag
+        // true : currentScore++
+        // false : currentScore = 0
+        
+        // update score label
+        
+        switch dot.tag {
+            
+        case 0: print("top left")
+        case 1: print("top right")
+        case 2: print("bottom left")
+        case 3: print("bottom right")
+        default: print("bad touch")
+            
+        }
+    
+    }
+    
+    func pingDot() {
+        
+        // choose random dot
+        
+        // make dot flash another color
+        
+        // set correctDot to the random dot
+        
+    }
+}
